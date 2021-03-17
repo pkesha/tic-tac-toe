603990c63d224922343e26a4d68983e730471a65
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-03-17 15:50:45 -0500

    Fixed error for event listener - but the fucntion reference is still using 'clicked' whenever is called

--- script.js
+++ script.js
@@ -4,59 +4,42 @@ class Game {
         this.player1 = true;
         this.player2 = false;
         this.boardSquare = [];
+        this.positionStorage = [];
         this.initialize();
     }
 
-    turns() {
+    //Will be used to determine who went and which
+    turns(position) {
         if(this.player1 === true) {
-            this.boardSquare.player = 1;
+            //Need to store where a player has been used
+            this.positionStorage[position] = 1;
             this.player1 = false;
             this.player2 = true;
             console.log("player 1");
         } else if (this.player2 === true) {
-            this.boardSquare.player = 2;
+            this.positionStorage[position] = 2;
             this.player1 = true;
             this.player2 = false;
             console.log("player 2");
         } else {
             //throw error
         }
+        console.log("End of Turns");
     }
 
     initialize() {
+        //console.log("Initialize Method");
         Array.from(this.board.children).forEach((child) => {
             this.boardSquare.push(child);
             let square = this.boardSquare.indexOf(child);
-            //Adding events to each square
-            this.boardSquare.addEventListener('click', function(event) {
-                console.log("adding event listeners")
-                this.turns()
+            let turnsCallback = this.turns(square);
+            this.boardSquare[square].addEventListener('click', function(event) {
+                event.preventDefault();
+                //They are not being clicked but still being used
+                turnsCallback;
             });
         });
     }
-
-    //
-
-
-    // checkForWinner(j) {
-    //     let three = 0;
-    //
-    //
-    //     //Checking Diagonals
-    //     let diagonal = [-8, -4, 4, 8];
-    //     for(let i = 0; i < 4; i++) {
-    //         if (this.boardSquare.indexOf(j).player === this.boardSquare.indexOf(diagonal[i])) {
-    //             three += 1;
-    //         }
-    //     }
-    //
-    //     if(three >= 3){
-    //         console.log("winner");
-    //     }
-    //
-    // }
-}
-
 }
 
 game = new Game();
