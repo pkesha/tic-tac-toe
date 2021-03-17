896cc1ccc0d96ca102d16f74a74a2a89c1eb1ead
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-03-17 14:39:01 -0500

    Removed turn method, added Initialize and commented logic

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ class Game {
         this.player1 = true;
         this.player2 = false;
         this.boardSquare = [];
+        this.initialize();
         this.turn();
+    }
+
+    initialize() {
+        Array.from(this.board.children).forEach((child) => {
+            this.boardSquare.push(child);
+            let square = this.boardSquare.indexOf(child);
+        });
     }
 
     checkForWinner(j) {
@@ -15,34 +23,34 @@ class Game {
         let diagonal = [-8, -4, 4, 8];
         for(let i = 0; i < 4; i++) {
             if (this.boardSquare.indexOf(j).player === this.boardSquare.indexOf(diagonal[i])) {
-
+                three += 1;
             }
+        }
+
+        if(three >= 3){
+            console.log("winner");
         }
 
     }
 
-    turn() {
-        let playerToCheck = 0;
-        Array.from(this.board.children).forEach((child) => {
-            this.boardSquare.push(child);
-            let square = this.boardSquare.indexOf(child);
-            this.boardSquare[square].addEventListener('click', function (event) {
-                event.preventDefault();
-                if(this.player1 === true) {
-                    this.boardSquare.player = 1;
-                    this.player1 = false;
-                    this.player2 = true;
-                } else if (this.player2 === true) {
-                    this.boardSquare.player = 2;
-                    this.player1 = true;
-                    this.player2 = false;
-                } else {
-                    //throw error
-                }
-            });
+                // if(this.player1 === true) {
+                //     this.boardSquare.player = 1;
+                //     this.player1 = false;
+                //     this.player2 = true;
+                //     console.log("player 1");
+                // } else if (this.player2 === true) {
+                //     this.boardSquare.player = 2;
+                //     this.player1 = true;
+                //     this.player2 = false;
+                //     console.log("player 2");
+                // } else {
+                //     //throw error
 
-            this.checkForWinner(square, playerToCheck);
-        });
-    }
-}
+//     this.boardSquare[square].addEventListener('click', function (event) {
+//     event.preventDefault();
+// });
+//     this.checkForWinner(square, playerToCheck);
+
+game = new Game();
+game;
 
