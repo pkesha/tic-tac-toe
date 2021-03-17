5ec8b4694a15b6af1f96487c5e2d39a675eb11da
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-03-17 17:34:16 -0500

    winner method for checking winners is complete.  Some testing but mostly styling from here on out

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ class Game {
         this.player1 = true;
         this.player2 = false;
         this.boardSquare = [];
-        this.positionStorage = [];
+        this.positionStorage = [0, 0, 0, 0, 0, 0, 0, 0, 0];
         this.initialize();
     }
 
@@ -29,23 +29,59 @@ class Game {
         if(this.player1 === true) {
             //Need to store where a player has been used
             this.positionStorage[position] = 1;
+            console.log(this.positionStorage);
             this.player1 = false;
             this.player2 = true;
-            console.log("player 1");
+            this.checkWinner(1, position);
+            //console.log("player 1");
         } else if (this.player2 === true) {
             this.positionStorage[position] = 2;
+            console.log(this.positionStorage);
             this.player1 = true;
             this.player2 = false;
-            console.log("player 2");
+            this.checkWinner(2, position);
+            //console.log("player 2");
         } else {
             //throw error
         }
     };
 
-    winner() {
+    checkWinner(player, position) {
         let diagonals = [-8, -4, 4, 8];
-        let row = [-2, -1, 1, 2];
+        let rows = [-2, -1, 1, 2];
         let columns = [-6, -3, 3, 6];
+
+        let rowScore = 0;
+        let columnScore = 0;
+        let diagonalScore = 0;
+
+        for(let i = 0; i < 5; i++){
+            if(player === this.positionStorage[position + diagonals[i]]){
+                //console.log(this.positionStorage[position + diagonals[i]]);
+                diagonalScore += 1;
+            }
+            if(player === this.positionStorage[position + rows[i]]) {
+                //console.log(this.positionStorage[position + rows[i]]);
+                rowScore += 1;
+            }
+            if(player === this.positionStorage[position + columns[i]]) {
+                //console.log(this.positionStorage[position + columns[i]]);
+                columnScore += 1;
+            }
+
+
+            if((rowScore === 2) ||
+                (columnScore === 2) ||
+                (diagonalScore === 2)) {
+                alert("Player " + player + " is the winner");
+                break;
+            }
+        }
+
+        console.log("D: ", diagonalScore);
+        console.log("C: ", columnScore);
+        console.log("R: ", rowScore);
+    }
 
 }
 
