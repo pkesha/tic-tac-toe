b425a8de61aefb200d3e2a6d9c0f2f901ff16ed4
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-03-17 14:46:19 -0500

    Added turn function.  It will be used in the initalize method for the event listener

--- script.js
+++ script.js
@@ -5,51 +5,59 @@ class Game {
         this.player2 = false;
         this.boardSquare = [];
         this.initialize();
-        this.turn();
+    }
+
+    turns() {
+        if(this.player1 === true) {
+            this.boardSquare.player = 1;
+            this.player1 = false;
+            this.player2 = true;
+            console.log("player 1");
+        } else if (this.player2 === true) {
+            this.boardSquare.player = 2;
+            this.player1 = true;
+            this.player2 = false;
+            console.log("player 2");
+        } else {
+            //throw error
+        }
     }
 
     initialize() {
         Array.from(this.board.children).forEach((child) => {
             this.boardSquare.push(child);
             let square = this.boardSquare.indexOf(child);
+            //Adding events to each square
+            this.boardSquare.addEventListener('click', function(event) {
+                console.log("adding event listeners")
+                this.turns()
+            });
         });
     }
 
-    checkForWinner(j) {
-        let three = 0;
+    //
 
 
-        //Checking Diagonals
-        let diagonal = [-8, -4, 4, 8];
-        for(let i = 0; i < 4; i++) {
-            if (this.boardSquare.indexOf(j).player === this.boardSquare.indexOf(diagonal[i])) {
-                three += 1;
-            }
-        }
+    // checkForWinner(j) {
+    //     let three = 0;
+    //
+    //
+    //     //Checking Diagonals
+    //     let diagonal = [-8, -4, 4, 8];
+    //     for(let i = 0; i < 4; i++) {
+    //         if (this.boardSquare.indexOf(j).player === this.boardSquare.indexOf(diagonal[i])) {
+    //             three += 1;
+    //         }
+    //     }
+    //
+    //     if(three >= 3){
+    //         console.log("winner");
+    //     }
+    //
+    // }
+}
 
-        if(three >= 3){
-            console.log("winner");
-        }
-
-    }
-
-                // if(this.player1 === true) {
-                //     this.boardSquare.player = 1;
-                //     this.player1 = false;
-                //     this.player2 = true;
-                //     console.log("player 1");
-                // } else if (this.player2 === true) {
-                //     this.boardSquare.player = 2;
-                //     this.player1 = true;
-                //     this.player2 = false;
-                //     console.log("player 2");
-                // } else {
-                //     //throw error
-
-//     this.boardSquare[square].addEventListener('click', function (event) {
-//     event.preventDefault();
-// });
-//     this.checkForWinner(square, playerToCheck);
+}
 
 game = new Game();
 game;
